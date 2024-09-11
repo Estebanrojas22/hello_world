2d04cf5cf39c56fb8631887a3fb062e71be518f8
Estebanrojas22 <Simei_roja@soy.sena.edu.co>
2024-09-11 16:49:23 -0500

    se modifica el archivo prueba.py

--- prueba.py
+++ prueba.py
@@ -4,6 +4,6 @@ print("hola") # Esta linea imprime en consola la palabra hola.
 time.sleep(3)
 print("hola despues de segundos")
 
-print (type("hola")) # La funcion 'type' me dice que tipo de dato hay en los parantesis.
+print (type("hola")) ## La funcion 'type' me dice que tipo de dato hay en los parantesis.
 print(type(9))
 print(type(5.5))
