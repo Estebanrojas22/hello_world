6e82721d36a3754962869a4d640e695cec2420f4
Estebanrojas22 <Simei_roja@soy.sena.edu.co>
2024-09-11 16:45:32 -0500

    se agrego un archivo prueba2.py

--- prueba.py
+++ prueba.py
@@ -1,5 +1,9 @@
 import time
 
-print("hola")
-time.sleep(10)
+print("hola") # Esta linea imprime en consola la palabra hola. 
+time.sleep(3)
 print("hola despues de segundos")
+
+print (type("hola")) # La funcion 'type' me dice que tipo de dato hay en los parantesis.
+print(type(9))
+print(type(5.5))
